2d0929b45a715b467c933873a2eb91286cbe2c91
changkong <2248392717@qq.com>
2023-10-19 07:26:46 +0200

    add the apis

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,19 +1,30 @@
-import { Skeleton } from '@/app/components';
-import { Box, Card, Flex } from '@radix-ui/themes';
+import prisma from '@/prisma/client';
+import { Box, Grid } from '@radix-ui/themes';
+import { notFound } from 'next/navigation';
+import EditIssueButton from './EditIssueButton';
+import IssueDetails from './IssueDetails';
 
-const LoadingIssueDetailPage = () => {
-  return (
-    <Box className='max-w-xl'>
-      <Skeleton />
-      <Flex className="space-x-3" my="2">
-        <Skeleton width="5rem" />
-        <Skeleton width="8rem" />
-      </Flex>
-      <Card className='prose' mt="4">
-        <Skeleton count={3} />
-      </Card>
-    </Box>
-  )
+interface Props {
+  params: { id: string };
 }
 
-export default LoadingIssueDetailPage
+const IssueDetailPage = async ({ params }: Props) => {
+  const issue = await prisma.issue.findUnique({
+    where: { id: parseInt(params.id) },
+  });
+
+  if (!issue) notFound();
+
+  return (
+    <Grid columns={{ initial: "1", md: "2" }} gap="5">
+      <Box>
+        <IssueDetails issue={issue} />
+      </Box>
+      <Box>
+       <EditIssueButton issueId={issue.id} />
+      </Box>
+    </Grid>
+  );
+};
+
+export default IssueDetailPage;
