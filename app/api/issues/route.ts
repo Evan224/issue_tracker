95ee13c38ee28d3fb336e113f7ff898430f499be
changkong <2248392717@qq.com>
2023-10-20 10:14:41 +0200

    add the function

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -1,50 +1,24 @@
-import { issueSchema } from "@/app/validationSchemas";
-import prisma from "@/prisma/client";
 import { NextRequest, NextResponse } from "next/server";
+import prisma from "@/prisma/client";
+import { issueSchema } from "../../validationSchemas";
+import { getServerSession } from "next-auth";
+import authOptions from "@/app/auth/authOptions";
 
-export async function PATCH(
-  request: NextRequest,
-  { params }: { params: { id: string } },
-) {
+export async function POST(request: NextRequest) {
+  const session = await getServerSession(authOptions);
+  if (!session) {
+    return NextResponse.json({}, { status: 401 });
+  }
+
   const body = await request.json();
   const validation = issueSchema.safeParse(body);
   if (!validation.success) {
     return NextResponse.json(validation.error.format(), { status: 400 });
   }
 
-  const issue = await prisma.issue.findUnique({
-    where: { id: parseInt(params.id) },
-  });
-  if (!issue) {
-    return NextResponse.json({ error: "Invalid issue" }, { status: 404 });
-  }
-
-  const updatedIssue = await prisma.issue.update({
-    where: { id: issue.id },
-    data: {
-      title: body.title,
-      description: body.description,
-    },
+  const newIssue = await prisma.issue.create({
+    data: { title: body.title, description: body.description },
   });
 
-  return NextResponse.json(updatedIssue);
-}
-
-export async function DELETE(
-  request: NextRequest,
-  { params }: { params: { id: string } },
-) {
-  const issue = await prisma.issue.findUnique({
-    where: { id: parseInt(params.id) },
-  });
-
-  if (!issue) {
-    return NextResponse.json({ error: "Invalid issue" }, { status: 404 });
-  }
-
-  await prisma.issue.delete({
-    where: { id: issue.id },
-  });
-
-  return NextResponse.json({});
+  return NextResponse.json(newIssue, { status: 201 });
 }
