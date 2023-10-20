ca8a73176dad6a35fb334b0bb818704a7a59a7f0
changkong <2248392717@qq.com>
2023-10-20 09:19:11 +0200

    change the cureent order

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -1,17 +1,50 @@
-import { NextRequest, NextResponse } from "next/server";
+import { issueSchema } from "@/app/validationSchemas";
 import prisma from "@/prisma/client";
-import { issueSchema } from "../../validationSchemas";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function POST(request: NextRequest) {
+export async function PATCH(
+  request: NextRequest,
+  { params }: { params: { id: string } },
+) {
   const body = await request.json();
   const validation = issueSchema.safeParse(body);
   if (!validation.success) {
     return NextResponse.json(validation.error.format(), { status: 400 });
   }
 
-  const newIssue = await prisma.issue.create({
-    data: { title: body.title, description: body.description },
+  const issue = await prisma.issue.findUnique({
+    where: { id: parseInt(params.id) },
+  });
+  if (!issue) {
+    return NextResponse.json({ error: "Invalid issue" }, { status: 404 });
+  }
+
+  const updatedIssue = await prisma.issue.update({
+    where: { id: issue.id },
+    data: {
+      title: body.title,
+      description: body.description,
+    },
   });
 
-  return NextResponse.json(newIssue, { status: 201 });
+  return NextResponse.json(updatedIssue);
+}
+
+export async function DELETE(
+  request: NextRequest,
+  { params }: { params: { id: string } },
+) {
+  const issue = await prisma.issue.findUnique({
+    where: { id: parseInt(params.id) },
+  });
+
+  if (!issue) {
+    return NextResponse.json({ error: "Invalid issue" }, { status: 404 });
+  }
+
+  await prisma.issue.delete({
+    where: { id: issue.id },
+  });
+
+  return NextResponse.json({});
 }
